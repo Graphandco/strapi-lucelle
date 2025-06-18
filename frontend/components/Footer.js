26c85943d66ac9771d7bacbc0d5541b07ea76f30
Régis Daum <rdaum@john-steel.com>
2025-06-18 12:24:31 +0200

    Changed footer and API limits

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -2,37 +2,60 @@
 
 import Link from "next/link";
 import { useAuth } from "@/contexts/AuthContext";
-import { ShoppingCart, Store, CirclePlus, CircleUser } from "lucide-react";
+import {
+   ShoppingCart,
+   Store,
+   CirclePlus,
+   CircleUser,
+   Home,
+} from "lucide-react";
+import { usePathname } from "next/navigation";
 
 export default function Footer() {
    const { user } = useAuth();
-
+   const pathname = usePathname();
    if (!user) return null;
 
    return (
       <footer className="fixed z-10 bottom-0 left-0 w-full bg-black p-3">
          <div className=" mx-auto flex justify-around items-center">
             <Link
-               href="/shopping-list"
-               className="flex flex-col items-center gap-1 px-2"
+               href="/"
+               className={`flex flex-col items-center gap-1 px-2 ${
+                  pathname === "/" ? "text-primary" : "text-white"
+               }`}
             >
-               <ShoppingCart className="w-6 h-6 text-white" />
-               <span className="text-white text-[10px]">Panier</span>
+               <Home className="w-6 h-6" />
+               <span className="text-[10px]">Accueil</span>
             </Link>
             <Link
-               href="/add-product"
-               className="flex flex-col items-center gap-1 px-2"
+               href="/shopping-list"
+               className={`flex flex-col items-center gap-1 px-2 ${
+                  pathname === "/shopping-list" ? "text-primary" : "text-white"
+               }`}
             >
-               <CirclePlus size={25} className="z-10" />
-               <span className="text-white text-[10px]">Ajouter</span>
+               <ShoppingCart className="w-6 h-6" />
+               <span className="text-[10px]">Panier</span>
             </Link>
             <Link
                href="/inventaire"
-               className="flex flex-col items-center gap-1 px-2"
+               className={`flex flex-col items-center gap-1 px-2 ${
+                  pathname === "/inventaire" ? "text-primary" : "text-white"
+               }`}
             >
-               <Store className="w-6 h-6 text-white" />
-               <span className="text-white text-[10px]">Inventaire</span>
+               <Store className="w-6 h-6" />
+               <span className="text-[10px]">Inventaire</span>
             </Link>
+            <Link
+               href="/add-product"
+               className={`flex flex-col items-center gap-1 px-2 ${
+                  pathname === "/add-product" ? "text-primary" : "text-white"
+               }`}
+            >
+               <CirclePlus size={25} className="z-10" />
+               <span className="text-[10px]">Ajouter</span>
+            </Link>
+
             {/* <Link
                href="/dashboard"
                className="flex flex-col items-center gap-1 px-2"
