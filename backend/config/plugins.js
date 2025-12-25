42fe446479a8bb5eb0f2a37670f3e4e5245edf56
Régis Daum <contact@graphandco.com>
2025-12-25 17:08:33 +0100

    Enhance middleware and plugin configurations: increase body size limits to 100MB in middlewares.js and set local upload provider with size limit in plugins.js.

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,1 +1,15 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'local',
+      providerOptions: {
+        sizeLimit: 100 * 1024 * 1024, // 100MB
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
